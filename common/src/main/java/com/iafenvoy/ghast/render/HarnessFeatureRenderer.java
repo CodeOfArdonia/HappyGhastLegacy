e11a3fb26a1a6bb76d73b6f5de7fdd2c4fdca0ec
IAFEnvoy <tanksheng545@126.com>
2025-04-27 19:29:30 +0800

    split models

--- common/src/main/java/com/iafenvoy/ghast/render/HarnessFeatureRenderer.java
+++ common/src/main/java/com/iafenvoy/ghast/render/HarnessFeatureRenderer.java
@@ -16,21 +16,23 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 class HarnessFeatureRenderer extends FeatureRenderer<HappyGhastEntity, GhastEntityModel<HappyGhastEntity>> {
-    public HarnessFeatureRenderer(FeatureRendererContext<HappyGhastEntity, GhastEntityModel<HappyGhastEntity>> ctx) {
+    private final HappyGhastHarnessEntityModel model;
+
+    public HarnessFeatureRenderer(FeatureRendererContext<HappyGhastEntity, GhastEntityModel<HappyGhastEntity>> ctx, HappyGhastHarnessEntityModel model) {
         super(ctx);
+        this.model = model;
     }
 
     @Override
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, HappyGhastEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         ItemStack stack = entity.getBodyArmor();
         if (!stack.isEmpty() && !entity.isBaby() && stack.getItem() instanceof HarnessItem harness) {
-            HappyGhastHarnessEntityModel model = new HappyGhastHarnessEntityModel(HappyGhastHarnessEntityModel.getTexturedModelData().createModel());
             VertexConsumer vertexConsumer = vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(Identifier.of(HappyGhastLegacy.MOD_ID, "textures/entity/equipment/%s_harness.png".formatted(harness.getColor().asString()))));
             matrices.push();
             matrices.scale(1.05F, 1.05F, 1.05F);
             matrices.translate(0.0F, 0.05F, 0.0F);
-            model.setAngles(entity, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
-            model.render(matrices, vertexConsumer, light, LivingEntityRenderer.getOverlay(entity, 0), 1, 1, 1, 1);
+            this.model.setAngles(entity, limbAngle, limbDistance, animationProgress, headYaw, headPitch);
+            this.model.render(matrices, vertexConsumer, light, LivingEntityRenderer.getOverlay(entity, 0), 1, 1, 1, 1);
             matrices.pop();
         }
     }
